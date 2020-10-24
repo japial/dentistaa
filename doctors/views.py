94b80115777e3478a4c19bae5ee21af38c645b5c
Jahangir Pial <pial@mpower-social.com>
2020-10-25 00:07:01 +0600

    Dentist Registration Form

--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate
 
-# Create your views here.
-# class DoctorRegister(CreateView):
+
+def index(request: HttpRequest):
+    return redirect('/')
+
+
+def register(request: HttpRequest):
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        return redirect('doctors/success')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration.html', {'form': form})
+
+
+def success(request: HttpRequest):
+    return render(request, 'success.html')
--- website/urls.py
+++ website/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from website import views
 
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('doctors/', include('doctors.urls')),
 ]
